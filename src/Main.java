b1b09ac6176e01d49db2b20abe966f527996a1e4
Fomenko-Denis <fomenko.roust@mail.ru>
2023-04-01 13:12:26 +0800

    Initial

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13600;
+        int pricePerMile = 20;
+        int bonusPerMile = 1;
+        int amountBonus;
+        amountBonus = ticketPrice / pricePerMile * bonusPerMile;
+        System.out.println(amountBonus);
+
+
+    }
+
+}
